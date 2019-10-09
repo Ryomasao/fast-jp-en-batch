f195acd73f3d42b341bbce68f6ee0263614cbe0e
tohu.masao <konoemario@gmail.com>
2019-10-09 23:07:17 +0900

    一括書き込み

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,36 @@
-import admin from 'firebase-admin';
+import admin from 'firebase-admin'
+import data from './data.json'
 
 admin.initializeApp({
-  credential: admin.credential.cert('key.json')
-});
+  credential: admin.credential.cert('key.json'),
+})
 
-let db = admin.firestore();
+const db = admin.firestore()
 
-db.collection('sentences').get()
-  .then(snapshopt => {
-    snapshopt.forEach(doc => {
-      console.log(doc.id, '=>', doc.data())
-    })
+const { doc } = data
+const batch = db.batch()
+
+doc.forEach(sentence => {
+  const ref = db.collection('sentences').doc()
+  batch.set(ref, sentence)
+})
+
+batch
+  .commit()
+  .then(() => {
+    console.log('finish')
   })
-  .catch(err => {
-    console.log(err)
+  .catch(error => {
+    console.log('error')
   })
+
+//db.collection('sentences')
+//  .get()
+//  .then(snapshopt => {
+//    snapshopt.forEach(doc => {
+//      console.log(doc.id, '=>', doc.data())
+//    })
+//  })
+//  .catch(err => {
+//    console.log(err)
+//  })
