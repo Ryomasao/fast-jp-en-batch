0f66ddec2f2c26e6514eb26cf936828ca3e4fa40
tohu.masao <konoemario@gmail.com>
2019-10-12 19:25:50 +0900

    追加機能と削除機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,73 @@
 import admin from 'firebase-admin'
 import data from './data.json'
 
-admin.initializeApp({
-  credential: admin.credential.cert('key.json'),
-})
-
-const db = admin.firestore()
-
-const { doc } = data
-const batch = db.batch()
-
-doc.forEach(sentence => {
-  const ref = db.collection('sentences').doc()
-  batch.set(ref, sentence)
-})
-
-batch
-  .commit()
-  .then(() => {
-    console.log('finish')
+const init = (): FirebaseFirestore.Firestore => {
+  admin.initializeApp({
+    credential: admin.credential.cert('key.json'),
   })
-  .catch(error => {
-    console.log('error')
+  return admin.firestore()
+}
+
+const addDoc = (db: FirebaseFirestore.Firestore): void => {
+  const { doc } = data
+  const batch = db.batch()
+
+  doc.forEach(sentence => {
+    const ref = db.collection('sentences').doc()
+    batch.set(ref, sentence)
   })
+
+  batch
+    .commit()
+    .then(() => {
+      console.log('finish')
+    })
+    .catch(error => {
+      console.log('add data was failed.')
+      throw error
+    })
+}
+
+const deleteDoc = async (db: FirebaseFirestore.Firestore): Promise<void> => {
+  try {
+    const snapshot = await db.collection('sentences').get()
+
+    if (snapshot.size === 0) {
+      return
+    }
+
+    const batch = db.batch()
+    snapshot.docs.forEach(doc => batch.delete(doc.ref))
+    await batch.commit()
+  } catch (e) {
+    console.log('delete doc failed')
+    throw e
+  }
+}
+
+type operation = 'add' | 'get' | 'delete'
+
+const main = (): void => {
+  if (process.argv.length < 3) {
+    throw new Error('you need an argument [add|get|delete]')
+  }
+
+  const operation: operation = process.argv[2] as operation
+  const db = init()
+
+  switch (operation) {
+    case 'add':
+      addDoc(db)
+      break
+    case 'delete':
+      deleteDoc(db)
+      break
+    default:
+      throw new Error(`your arugment '${operation}' is wrong `)
+  }
+}
+
+main()
 
 //db.collection('sentences')
 //  .get()
